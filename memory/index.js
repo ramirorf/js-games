7edfc4c20612fee9e3866d3abb7eb2caefb5e82f
Ramiro Robles <ramisbaker@gmail.com>
2023-05-21 01:30:31 +0200

    correcciones

--- memory/index.js
+++ memory/index.js
@@ -1,10 +1,20 @@
+
 function init() {
     const boardElement = document.querySelector('.board');
 
     // Crear las celdas del tablero
     for (let i = 0; i < 30; i++) {
         const cell = document.createElement('div');
-        cell.className = 'cell row-' + Math.floor(i/6) + ' col-' + i%6; 
+        cell.className = 'cell row-' + Math.floor(i/6) + ' col-' + i%6 + ' cell-hide';
+        cell.addEventListener('click', () => cellClick(cell));
         boardElement.appendChild(cell);
+    }
+}
+
+function cellClick(cell) {
+    if(cell.classList.contains("cell-hide")) {
+        cell.classList.remove("cell-hide");
+    } else {
+        cell.classList.add("cell-hide");
     }
 }
