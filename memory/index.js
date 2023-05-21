d0c338585b053a39e079b254b5297a181720eabe
Ramiro Robles <ramisbaker@gmail.com>
2023-05-21 02:34:11 +0200

    correcciones

--- memory/index.js
+++ memory/index.js
@@ -8,7 +8,8 @@ function init() {
     // Crear las celdas del tablero
     for (let i = 0; i < 30; i++) {
         const cell = document.createElement('div');
-        cell.className = 'cell row-' + Math.floor(i/6) + ' col-' + i%6 + ' cell-hide';
+//        cell.className = 'cell row-' + Math.floor(i/6) + ' col-' + i%6 + ' cell-hide';
+        cell.className = 'cell row-' + 0 + ' col-' + 0 + ' cell-hide';
         cell.addEventListener('click', () => cellClick(cell));
         boardElement.appendChild(cell);
 
@@ -24,13 +25,43 @@ function cellClick(cell) {
         if(celda_seleccionada == null) {
             celda_seleccionada = cell;
         } else {
-            setTimeout(() => {
-                cell.classList.add("cell-hide");
-                celda_seleccionada.classList.add("cell-hide");
-                celda_seleccionada = null;
-            }, 500)
+
+            // comprobar si es la misma imagen
+            if(mismaImagen(celda_saellecionada, cell)) {       
+                
+                // si todas están descubiertas
+                if(document.getElementsByClassName('cell-hide')[0].length == 0) {
+                    alert('¡¡Has ganado!!');
+                    
+                    // dejar el estado como inicialmente
+                    celda_seleccionada = null;
+                    document.getElementsByClassName("cell")[0].forEach(function() {
+                        this.classList.add("cell-hide");
+                    });
+                }
+            } else {
+                // dejar el tablero como al principio del turno
+                setTimeout(() => {
+                    cell.classList.add("cell-hide");
+                    celda_seleccionada.classList.add("cell-hide");
+                    celda_seleccionada = null;
+                }, 500);
+            }
         }
     } 
 }
 
+function mismaImagen(celda1, celda2) {
+    var row = null;
+    var col = null;
 
+    celda1.classList.forEach(function() {
+        if (this.startsWith('col-')) {
+            col = this;
+        } else if (this.startsWith('row-')) {
+            row = this;
+        }
+    });
+
+    return celda2.classList.contains(col) && celda2.classList.contains(row);
+}
